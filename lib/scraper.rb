732d7e2597a5fffca80b619d3190759cd7fa399e
jolly-technologist-1233 <pbrennanmusic@gmail.com>
2021-04-10 20:20:02 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,7 +3,7 @@ require 'open-uri'
 
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 
-courses = doc.css(".text-4GLMvr.")
+courses = doc.css(".text-4GLMvr.title-oE5vT4")
 
 courses.each do |course|
   puts course.text.strip
